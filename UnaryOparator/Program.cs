214f3ab532571b078cb3dad7185da025e4f1c1d7
sayalihandore111 <sayalihandore111@gmail.com>
2021-08-31 20:41:55 +0530

    Add project files.

---
+++ UnaryOparator/Program.cs
@@ -0,0 +1,34 @@
+﻿
+using System;
+
+namespace UnaryOperatorsDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b;
+            Console.WriteLine("Assigning value to a and b");
+            a = b = 15;
+            Console.WriteLine("a= " + a);//a = 15
+            Console.WriteLine("b= " + b);//b = 15
+            Console.WriteLine("Post incrementing a & assinging its value to b");
+            b = a++;
+            Console.WriteLine("a= " + a);//a = 16
+            Console.WriteLine("b= " + b);//b = 15
+            Console.WriteLine("Post decrementing a & assinging its value to b");
+            b = a--;
+            Console.WriteLine("a= " + a);//a = 15
+            Console.WriteLine("b= " + b);//b = 16
+            Console.WriteLine("Pre decrementing a & assinging its value to b");
+            b = ++a;
+            Console.WriteLine("a= " + a);//a = 16
+            Console.WriteLine("b= " + b);//a = 16
+            Console.WriteLine("Pre decrementing a & assinging its value to b");
+            b = --a;
+            Console.WriteLine("a= " + a);//a = 15
+            Console.WriteLine("b= " + b);//b = 15
+            Console.ReadKey();
+        }
+    }
+}
